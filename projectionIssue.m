2d88df7f45fda0ae3bdd19947ddbfb5b163a82bb
scharalambous3 <scharalambou@student.ethz.ch>
2022-12-28 19:22:26 +0200

    code cleanup

--- projectionIssue.m
+++
@@ -1,8 +0,0 @@
-clear;
-clc;
-load('projectionIssue.mat');
-%load('projectionIssueModified.mat');
-[Z_k, objValue] = solveConvexSubproblem(Delta_k, P_k, G_k, x_k, params);
-
-%%
-[Delta_k, int_k] = projectionSubproblem(Z_k, P_k, G_k, params);
